4a291762b772799abc947573a51aeec3b453b170
Falkonny <62031372+Falkonny@users.noreply.github.com>
2020-12-05 13:03:11 +0300

    Validation match the task

--- MoviesApp/ViewModels/InputActorViewModel.cs
+++ MoviesApp/ViewModels/InputActorViewModel.cs
@@ -7,8 +7,12 @@ namespace MoviesApp.ViewModels
     public class InputActorViewModel
     {
         [Required]
+        [MinLength(4, ErrorMessage = "First name length can't be less than 4.")]
+        [StringLength(32, ErrorMessage = "First name length can't be more than 32.")]
         public string FirstName { get; set; }
         [Required]
+        [MinLength(4, ErrorMessage = "Last name length can't be less than 4.")]
+        [StringLength(32, ErrorMessage = "Last name length can't be more than 32.")]
         public string LastName { get; set; }
         [Required]
         [DataType(DataType.Date)]
