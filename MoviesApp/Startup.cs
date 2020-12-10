6bb50a730bdce75ad0b2b12e2f252668acde1619
Falkonny <62031372+Falkonny@users.noreply.github.com>
2020-12-10 15:32:55 +0300

    Database is working with actors

--- MoviesApp/Startup.cs
+++ MoviesApp/Startup.cs
@@ -1,12 +1,8 @@
-using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -19,12 +15,12 @@ namespace MoviesApp
 {
     public class Startup
     {
-        public IConfiguration Configuration { get; }
-
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
+
+        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
@@ -33,7 +29,8 @@ namespace MoviesApp
             services.AddControllersWithViews();
 
             services.AddDbContext<MoviesContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("MoviesContext")));
+                options.UseSqlServer(
+                    "Server=LAPTOP-V6M1QK29;Database=MoviesContext;Integrated security=True;MultipleActiveResultSets=true"));
 
             //Подключаем AutoMapper
             services.AddAutoMapper(typeof(Startup));
@@ -72,8 +69,8 @@ namespace MoviesApp
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    "default",
+                    "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
